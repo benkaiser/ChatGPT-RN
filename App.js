76f81ea71dbc4784987a01a7e86b7392d306e6ca
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2023-04-08 00:13:29 +1000

    Add initial app

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const ChatScreen = () => {
+  return <>
+    <WebView
+      source={{ uri: 'https://chat.openai.com/' }}
+      style={{ flex: 1 }}
+    />
+    <StatusBar style="light" backgroundColor="#343541" translucent={false} />
+  </>;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default ChatScreen;
